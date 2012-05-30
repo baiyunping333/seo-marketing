e5a77ae0906f8081070d93e3cdf6504fc9002924
occultknight@gmail.com <occultknight@gmail.com@80dd8c17-1622-8df1-d142-d14f9fca40e8>
2012-05-30 12:44:06 +0000

    SharpPoster

---
+++ trunk/tools/SharpPoster/SharpPoster/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Text;
+
+namespace SharpPoster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.Request();
+            }
+        }
+
+        private void Request()
+        {
+            string url = tbUrl.Text;
+
+            if (!(url.StartsWith("http://") || url.StartsWith("https://")))
+            {
+                url = "http://" + url;
+            }
+
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                var response = HttpWebResponseUtility.CreateGetHttpResponse(url, null, null, null);
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        tbResponse.Text = sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
