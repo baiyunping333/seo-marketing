b1b2aabed6e3bac2d9945fd690ba3f3a8cba404c
afeiship@gmail.com <afeiship@gmail.com@80dd8c17-1622-8df1-d142-d14f9fca40e8>
2012-09-11 08:18:22 +0000

    test!

--- trunk/sendMailForResume/sendMailForResume/Form1.cs
+++ trunk/sendMailForResume/sendMailForResume/Form1.cs
@@ -10,15 +10,16 @@ using System.Net.Mail;
 using System.IO;
 using System.Net;
 using System.Net.Mime;
+using System.Xml.Serialization;
 
 namespace sendMailForResume
 {
     public partial class MainFrm : Form
     {
         SmtpClient SmtpClient = new SmtpClient();   //设置SMTP协议
-        
 
 
+        MailInfo info = new MailInfo();
 
         public MainFrm()
         {
@@ -27,13 +28,23 @@ namespace sendMailForResume
         }
 
         public void initConfig() {
-            SmtpClient.Host = "smtp.qq.com";
+
+            
+            
+
+            SmtpClient.Host = "xchcasha.cisco.com";
             SmtpClient.Port = 25;
             SmtpClient.UseDefaultCredentials = false;
             SmtpClient.SendCompleted += new SendCompletedEventHandler(SmtpClient_SendCompleted);
         }
 
         public void SendMailBySmtp(string mail_from,string mail_fromPwd,string mail_to,string mail_subject,string mail_body,string attachment=null) {
+
+            info.From = mail_from;
+            info.To = mail_to;                     
+            info.Title = mail_subject;
+            info.Body = mail_body;
+
             SmtpClient.Credentials = new NetworkCredential(mail_from, mail_fromPwd);
             SmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             Byte[] b = Encoding.Default.GetBytes(mail_body);
@@ -66,7 +77,15 @@ namespace sendMailForResume
 
         private void sendMail_Click(object sender, EventArgs e)
         {
-            this.SendMailBySmtp("1290657123@qq.com","happyfox","88603982@qq.com","test2","好吧测试一下<b>NiuBi</b>!!!");
+            this.SendMailBySmtp("feizheng@cisco.com", "SK_123.com", "88603982@qq.com", "test2", "好吧测试一下<b>NiuBi</b>!!!");
+
+            XmlSerializer ser = new XmlSerializer(typeof(MailInfo));
+
+            using(Stream s = File.OpenWrite("c:\\abc.xml"))
+            {
+                ser.Serialize(s, info);
+            }
+            //this.SendMailBySmtp("feizheng@cisco.com","SK_123.com","88603982@qq.com","test2","好吧测试一下<b>NiuBi</b>!!!");
         }
 
 
